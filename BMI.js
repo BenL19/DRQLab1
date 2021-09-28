ef7d22010d55035f1ad71fa166c7dfea277228f7
BEN LARDNER - STUDENT <G00371775@gmit.ie>
2021-09-28 10:47:23 +0100

    Second Commit

--- BMI.js
+++ BMI.js
@@ -1,16 +1,23 @@
 class BMI
 {
+    //Create instances of height and weight
     constructor(height, weight){
         this.height = height;
         this.weight = weight;
     }
 
+    //Method to calculate BMI
     calculateBMI(){
         let bmi = this.weight/(this.height**2);
         return bmi;
     }
 }
 
+//Assigning the bmi to a variable
 let myBMI = new BMI(2, 90);
+
+//Assigning a variable to the calculation method
 let calc = myBMI.calculateBMI();
+
+//Printing the answer
 console.log(calc);
--- cars.js
+++ cars.js
@@ -1,16 +1,19 @@
 class Vehicle
 {
+    //Create instances of make, model, year
     constructor(make, model, year){
         this.make = make;
         this.model = model;
         this.year = year;
     }
 
+    //Method to print info on car
     Information(){
         console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
     }
 }
 
+//Class that inherits from vehicle, extends to add value for doors
 class Cars extends Vehicle {
     constructor(make, model, year, doors){
         super(make, model, year)
@@ -23,5 +26,6 @@ class Cars extends Vehicle {
     }
 }
 
+//Assigns variable for car with all info
 let myCar = new Cars('Ford', 'Fiesta', 2012, 4);
 myCar.Information();
